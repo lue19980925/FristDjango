be40ec9fea619cd5bddd79bd0a224ce50130832b
2773251565@qq.com <2773251565@qq.com>
2020-07-30 16:53:23 +0800

    Django第一次提交：后台管理，数据库管理

---
+++ FristDjango/__init__.py
@@ -0,0 +1,2 @@
+#import pymysql
+# MySQLdb只支持Python2.*，还不支持3.可以用PyMySQL代替。
---
+++ bookApp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    #title is a string leixing max_length is 20
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(True)
+    def __repr__(self):
+        return '<Book: %s>' %(self.title)
+
+    # 自定义对应的表名，默认表名：bookApp_book
+    class Meta:
+        db_table = "books"
+class Hero(models.Model):
+    name = models.CharField(max_length=20)     #name
+    gender = models.BooleanField()  #sex
+    content = models.CharField(max_length=200)  #jianjie
+    Book = models.ForeignKey('Book',on_delete=models.CASCADE)
+    def __repr__(self):
+        return "<Hero %s>" %(self.name)
+    # 自定义对应的表名，默认表名：bookApp_hero
+    class Meta:
+        db_table = "heros"
+
